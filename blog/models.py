008371fa7471ef64563717ffc5c3ca2bc25fb78f
wajiha-lodhi <wajihalodhi1111@gmail.com>
2019-06-29 05:00:59 +0100

    jobs are showing up Blogs are almost done

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=40)
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to='images/')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e, %Y')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
+
+
+def allblogs(request):
+    post = Blog.objects
+    return render(request, 'blog/allblogs.html', {'post': post})
