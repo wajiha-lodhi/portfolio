31da583389dbad77bb641921841ed53caecc55a3
wajiha-lodhi <wajihalodhi1111@gmail.com>
2019-06-30 20:14:49 +0100

    the portfolio and blog are done and properly working

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 # Create your views here.
 
@@ -6,3 +6,8 @@ from .models import Blog
 def allblogs(request):
     post = Blog.objects
     return render(request, 'blog/allblogs.html', {'post': post})
+
+
+def detail(request, blog_id):
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': detailblog})
